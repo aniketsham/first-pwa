f355b7294a7c020eea875b90d812b1664a5f2c3a
Aniket Sharma <89375574+aniketsham@users.noreply.github.com>
2025-01-14 19:34:40 +0530

    adding working offline changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [
     react(),
@@ -10,7 +10,7 @@ export default defineConfig({
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
       manifest: {
         name: "Vite PWA Project",
-        short_name: "Vite PWA Project",
+        short_name: "Vite PWA",
         theme_color: "#ffffff",
         icons: [
           {
@@ -34,6 +34,33 @@ export default defineConfig({
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable",
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /\/.*\.(?:js|css|html|json|png|jpg|jpeg|svg|ico|webp)$/,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "assets-cache",
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+              },
+            },
+          },
+          {
+            urlPattern: /^https:\/\/fakestoreapi\.com\/users/,
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "api-cache",
+              networkTimeoutSeconds: 10,
+              expiration: {
+                maxEntries: 20,
+                maxAgeSeconds: 7 * 24 * 60 * 60, // 7 Days
+              },
+            },
           },
         ],
       },
